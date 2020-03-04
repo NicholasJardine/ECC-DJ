901d63050774c55a1d0ff5bf4b799219ca714a2c
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-03-04 11:10:17 +0200

    Rails new with frontend and form gems

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
-
   def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def social_outreach
   end
 end
