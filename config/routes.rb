87ce7b3e53cb0da799809959ecadf9527944301b
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-03-04 11:32:54 +0200

    routes, models done

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get 'pages/social_outreach'
   devise_for :users
   root to: 'pages#home'
+
+  resources :categories, only: [:index, :show, :home, :art, :garden] do
+    resources :products, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
